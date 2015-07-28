d26cc796dd85da7913c43dc0b84ad999e7430366
hp <haixing6112008@126.com>
2015-07-28 09:17:46 +0800

    test3

--- ptr-demo/src/in/srain/cube/views/ptr/demo/app/PtrDemoApplication.java
+++ ptr-demo/src/in/srain/cube/views/ptr/demo/app/PtrDemoApplication.java
@@ -18,7 +18,7 @@ public class PtrDemoApplication extends Application {
     public void onCreate() {
         super.onCreate();
         instance = this;
-        System.out.print("test");  System.out.print("test2");
+        System.out.print("test");  System.out.print("test3");
         String environment = "";
 
         if (environment.equals("production")) {
